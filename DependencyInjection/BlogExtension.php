4dd50b6cf5e334033a4b1f266f3f73f7240d085e
shandyrov <shandyrov@gmail.com>
2019-03-26 14:19:24 +0500

    Update BlogExtension.php

--- DependencyInjection/BlogExtension.php
+++ DependencyInjection/BlogExtension.php
@@ -28,12 +28,13 @@ class BlogExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = $this->getConfiguration($configs, $container);
-        $this->processConfiguration($configuration, $configs);
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('routes.yaml');
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.xml');  
     }
-
-
 }
