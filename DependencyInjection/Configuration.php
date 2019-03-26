bad270be4d37020c051a2e5e705f2fbb39c96ba5
shandyrov <shandyrov@gmail.com>
2019-03-26 13:27:37 +0500

    Create Configuration.php

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,24 @@
+<?php
+namespace Sputnik\BlogBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ * Class Configuration
+ * @package App\Sputnik\BlogBundle\DependencyInjection
+ */
+class Configuration implements ConfigurationInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('blog');
+        return $treeBuilder;
+    }
+}
